97998d459c2d989ab95be0be0f98d41501ef7ca9
Kakha <kakhachankseliani@gmail.com>
2024-05-29 22:33:03 +0400

    feat: finished basic layout for backend: * added productController * added productRoutes

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,13 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const Product = require("./models/product.model");
+
+const productRoutes = require("./routes/product.routes");
 
 app.use(express.json());
+app.use("/api/products", productRoutes);
 
-app.get("/api/products", async (req, res) => {
-	try {
-		const products = await Product.getAll();
-		res.json(products);
-	} catch (error) {
-		console.log(error);
-	}
-});
-
-app.get("/api/products/:id", async (req, res) => {
-	const product = await Product.getById(req.params.id);
-	res.json(product);
-});
-
-app.post("/api/products", async (req, res) => {
-	await Product.add(req.body);
-	res.json("Product Added");
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-	await Product.remove(req.params.id);
-	res.json("Product deleted");
-});
+app.use(productRoutes);
 
 app.listen(port, () => {
 	console.log(`Server is running on port http://localhost:${port}`);
